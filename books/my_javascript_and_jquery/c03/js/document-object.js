0b9ef228678d6498ac3b2456c7ce24ce69bbf813
mumingv <mumingv@163.com>
2016-09-21 07:20:47 +0800

    add file

---
+++ books/my_javascript_and_jquery/c03/js/document-object.js
@@ -0,0 +1,7 @@
+// 在页面上显示document对象的一些属性
+var msg = '<p><b>page title: </b>' + document.title + '<br />';
+msg += '<b>page address: </b>' + document.URL + '<br />';
+msg += '<b>last modified: </b>' + document.lastModified + '</p>';
+
+var el = document.getElementById('footer');
+el.innerHTML = msg;
