1c3e3ec5b06eefaf0685ebf475b38b121f3448de
mumingv <mumingv@163.com>
2016-09-24 11:56:41 +0800

    add file

---
+++ books/my_javascript_and_jquery/c06/js/event-attributes.js
@@ -0,0 +1,11 @@
+function checkUsername() {
+    var elMsg = document.getElementById('feedback');
+    var elUsername = document.getElementById('username');
+    // elUsername是一个HTML DOM Text对象, 参考：
+    // http://www.w3school.com.cn/jsref/dom_obj_text.asp
+    if (elUsername.value.length < 5) {
+        elMsg.textContent = 'Username must be 5 characters or more';
+    } else {
+        elMsg.textContent = '';
+    }
+}
