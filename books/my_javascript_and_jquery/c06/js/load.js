9a61f2743995fb973e2ba91ca26f12588ae539c7
mumingv <mumingv@163.com>
2016-09-24 23:52:34 +0800

    add file

--- books/my_javascript_and_jquery/c06/js/load.js
+++ books/my_javascript_and_jquery/c06/js/load.js
@@ -1,4 +1,11 @@
 // 用户界面(UI)：load事件
 
+function setup() {
+    var textInput;
+    textInput = document.getElementById('username');
+    // 在文本域上设置焦点
+    textInput.focus();
+}
 
-
+// 使用事件监听器(函数没有参数): 当页面加载完成时触发load事件
+window.addEventListener('load', setup, false);
