3279dd1deffc40b6a2cb341deaaa3a0c4dbd8998
mumingv <mumingv@163.com>
2016-09-26 23:21:48 +0800

    add file

---
+++ books/my_javascript_and_jquery/c07/js/chaining.js
@@ -0,0 +1,5 @@
+// 链式操作
+
+$('li[id!="one"]').hide().delay(500).fadeIn(1400);  // 数字的单位是毫秒
+$('li:first-child').addClass('next');
+$('li.priority').addClass('highlight');
---
+++ books/my_javascript_and_jquery/c07/js/get-html-fragment.js
@@ -0,0 +1,6 @@
+// 获取元素内容
+
+$(function() {
+    var $listHTML = $('ul').html();
+    $('ul').append($listHTML);
+});
