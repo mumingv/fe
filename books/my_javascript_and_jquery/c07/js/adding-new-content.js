b6f3f88969de862e10dc94cb790444ba4c535914
mumingv <mumingv@163.com>
2016-09-27 00:12:37 +0800

    add file

---
+++ books/my_javascript_and_jquery/c07/js/adding-new-content.js
@@ -0,0 +1,8 @@
+// 添加新内容
+
+$(function() {
+    $('ul').before('<p class="notice">Just updated</p>');
+    $('li.hot').prepend('+ ');
+    var $newListItem = $('<li><em>gluten-free</em> soy sauce</li>');
+    $('li:last').after($newListItem);
+});
