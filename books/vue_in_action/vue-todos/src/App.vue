27aec57acc45a4cf12f50f09dcdbca45d5148694
mumingv <mumingv@163.com>
2017-08-01 00:15:10 +0800

    1.2 数据绑定

--- books/vue_in_action/vue-todos/src/App.vue
+++ books/vue_in_action/vue-todos/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="(todo, index) in todos" :id="index">
+        {{ index + 1 }}.{{ todo.value }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -9,7 +14,12 @@ export default {
   name: 'app',
   data () {
     return {
-      title: "vue-todos"    
+      title: "vue-todos",
+      todos: [
+        {value: "阅读一本关于前端开发的书", done: false},
+        {value: "补充范例代码", done: true},
+        {value: "写心得", done: false}
+      ]
     }
   }
 }
