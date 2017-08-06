8087a2a0d2854eeb61da49182a9fb8e67258d4ff
mumingv <mumingv@163.com>
2017-08-06 23:14:05 +0800

    add

--- video/imooc/vue/vue_platform/vuedemo2/src/main.js
+++ video/imooc/vue/vue_platform/vuedemo2/src/main.js
@@ -1,15 +1,11 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 引入库'vue'，import关键字是ES6的语法，类似于之前的require关键字
 import Vue from 'vue'
-import App from './App'
-import router from './router'
 
-Vue.config.productionTip = false
-
-/* eslint-disable no-new */
+// 实例化一个Vue对象，将Vue组件加入到页面上，这里的'new Vue'是一个根组件
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+	el: '#app',
+	template: "<p>hello world {{ word }}</p>",  // 原index.html页面中的div元素会被模版里的p元素替换
+	data: {
+		word: 'hello world2'  // 数据渲染，这里的word对应模版中的{{ word }}
+	}
 })
