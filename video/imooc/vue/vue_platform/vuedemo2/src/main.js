f2992d126c2f83a6d9bb4de8f811e5a6f49e845c
mumingv <mumingv@163.com>
2017-08-06 23:32:35 +0800

    注册全局组件

--- video/imooc/vue/vue_platform/vuedemo2/src/main.js
+++ video/imooc/vue/vue_platform/vuedemo2/src/main.js
@@ -1,10 +1,14 @@
 // 引入库'vue'，import关键字是ES6的语法，类似于之前的require关键字
 import Vue from 'vue'
 
+// 注册全局组件
+Vue.component('my-header', {
+	template: '<p>this is my header</p>'
+})
+
 // 实例化一个Vue对象，将Vue组件加入到页面上，这里的'new Vue'是一个根组件
 new Vue({
 	el: '#app',
-	template: "<p>hello world {{ word }}</p>",  // 原index.html页面中的div元素会被模版里的p元素替换
 	data: {
 		word: 'hello world2'  // 数据渲染，这里的word对应模版中的{{ word }}
 	}
