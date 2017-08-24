800cf7a288667f266e3d51011ddf0586c3bb1095
mumingv <mumingv@163.com>
2017-08-24 12:01:54 +0800

    首页 - express启动数据服务

--- video/imooc/vue/vue_platform/vuedemo3/build/dev-server.js
+++ video/imooc/vue/vue_platform/vuedemo3/build/dev-server.js
@@ -85,16 +85,36 @@ var server = app.listen(port)
 /*
  * json-server
  */
-var jsonServer = require('json-server')
-const apiServer = jsonServer.create()
-const apiRouter = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
-
-apiServer.use(middlewares)
-apiServer.use('/api', apiRouter)
-apiServer.listen(port + 1, () => {
-  console.log('JSON Server is running')
+var apiServer = express()
+var bodyParser = require('body-parser')
+apiServer.use(bodyParser.urlencoded({ extended: true }))
+apiServer.use(bodyParser.json())
+var apiRouter = express.Router()
+var fs = require('fs')
+apiRouter.route('/:apiName')
+.all(function (req, res) {
+  fs.readFile('./db.json', 'utf8', function (err, data) {
+    if (err) throw err
+    var data = JSON.parse(data)
+    if (data[req.params.apiName]) {
+      res.json(data[req.params.apiName])  
+    }
+    else {
+      res.send('no such api name')
+    }
+    
+  })
 })
+
+apiServer.use('/api', apiRouter);
+apiServer.listen(port + 1, function (err) {
+  if (err) {
+    console.log(err)
+    return
+  }
+  console.log('Listening at http://localhost:' + (port + 1) + '\n')
+})
+
 
 module.exports = {
   ready: readyPromise,
