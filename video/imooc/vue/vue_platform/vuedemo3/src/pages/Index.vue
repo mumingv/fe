e6efeaf35575092e39812b92dda9145e2db67026
mumingv <mumingv@163.com>
2017-09-26 21:37:20 +0800

    首页 - 幻灯片组件

--- video/imooc/vue/vue_platform/vuedemo3/src/pages/Index.vue
+++ video/imooc/vue/vue_platform/vuedemo3/src/pages/Index.vue
@@ -25,6 +25,7 @@
       </div>
     </div>
     <div class="index-right">
+      <slide-show :slides="slides" :inv="invTime"></slide-show>
       <div class="index-board-list">
         <div class="index-board-item"
         v-for="(item, index) in boardList"
@@ -43,7 +44,11 @@
 </template>
 
 <script>
+import SlideShow from '../components/SlideShow'
 export default {
+  components: {
+    SlideShow
+  },
   created: function() {
     this.$http.get('api/getNewsList')
     .then((res) => {
@@ -54,6 +59,29 @@ export default {
   },
   data () {
     return {
+      invTime: 2000,
+      slides: [
+        {
+          src: require('../assets/slideShow/pic1.jpg'),
+          title: 'xxx1',
+          href: 'detail/analysis'
+        },
+        {
+          src: require('../assets/slideShow/pic2.jpg'),
+          title: 'xxx2',
+          href: 'detail/count'
+        },
+        {
+          src: require('../assets/slideShow/pic3.jpg'),
+          title: 'xxx3',
+          href: 'http://xxx.xxx.com'
+        },
+        {
+          src: require('../assets/slideShow/pic4.jpg'),
+          title: 'xxx4',
+          href: 'detail/forecast'
+        }
+      ],
       boardList: [
         {
           title: '开放产品',
