7ce240722a33bcfc9f0e3bc12f2be113c876a67a
mumingv <mumingv@163.com>
2017-09-28 23:55:42 +0800

    add

---
+++ vue/element-ui/element-starter/src/Icon1.vue
@@ -0,0 +1,16 @@
+<template>
+	<div id="app">
+		<i class="el-icon-edit"></i>
+		<i class="el-icon-share"></i>
+		<i class="el-icon-delete"></i>
+		<el-button type="primary" icon="search">搜索</el-button>
+	</div>
+</template>
+
+<script>
+	
+</script>
+
+<style>
+	
+</style>
--- vue/element-ui/element-starter/src/main.js
+++ vue/element-ui/element-starter/src/main.js
@@ -2,10 +2,16 @@ import Vue from 'vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
 import App from './App.vue'
+import Layout1 from './Layout1.vue'
+import Icon1 from './Icon1.vue'
+import Radio1 from './Radio1.vue'
 
 Vue.use(ElementUI)
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  //render: h => h(App)
+  //render: h => h(Layout1)
+  //render: h => h(Icon1)
+  render: h => h(Radio1)
 })
