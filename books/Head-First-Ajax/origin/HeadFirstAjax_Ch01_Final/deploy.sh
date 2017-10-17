1d42ec452bd7efb4c2e7d02607d7202d147c61a0
mumingv <mumingv@163.com>
2017-10-17 10:57:40 +0800

    add

--- books/Head-First-Ajax/origin/HeadFirstAjax_Ch01_Final/deploy.sh
+++ books/Head-First-Ajax/origin/HeadFirstAjax_Ch01_Final/deploy.sh
@@ -2,16 +2,25 @@
 
 # 配置（使用者配置）
 NGINX_CONF_DIR=/usr/local/etc/nginx
+PORT=8081
 ROOT_PATH=`pwd`
 
+####################
 ### 以下不要修改 ###
+####################
 
 # 处理配置
 cp ../../backup/hf_ajax.conf ./hf_ajax.conf.temp
-sed 's#{ROOT_PATH}#'"$ROOT_PATH"'#g' hf_ajax.conf.temp > hf_ajax.conf
+sed 's#{PORT}#'"$PORT"'#g'';s#{ROOT_PATH}#'"$ROOT_PATH"'#g' hf_ajax.conf.temp > hf_ajax.conf
 mv hf_ajax.conf $NGINX_CONF_DIR/servers/
 rm hf_ajax.conf.temp
 
 # 重启Nginx
-nginx -s quit && sleep 1
-nginx
+nginx -s reload
+if [ $? -ne 0 ]; then
+    nginx && sleep 1
+fi
+
+# 打开页面
+open http://localhost:$PORT
+
