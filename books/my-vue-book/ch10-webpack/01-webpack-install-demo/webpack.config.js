f0139a2c044dc30cf9598f6463911fac253aafe1
mumingv <mumingv@163.com>
2018-02-26 00:12:16 +0800

    增加html和js文件

--- books/my-vue-book/ch10-webpack/01-webpack-install-demo/webpack.config.js
+++ books/my-vue-book/ch10-webpack/01-webpack-install-demo/webpack.config.js
@@ -1,4 +1,14 @@
+var path = require('path');
+
 var config = {
+    entry: {
+        main: './main'
+    },
+    output: {
+        path: path.join(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'main.js'
+    }
 };
 
 module.exports = config;
