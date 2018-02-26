f6e4543505f7265d3ed1d0188b278d1cc0a4c337
mumingv <mumingv@163.com>
2018-02-27 00:19:58 +0800

    10.4 用于生产环境

--- books/my-vue-book/ch10-webpack/01-webpack-install-demo/webpack.config.js
+++ books/my-vue-book/ch10-webpack/01-webpack-install-demo/webpack.config.js
@@ -35,6 +35,10 @@ var config = {
                     use: 'css-loader',
                     fallback: 'style-loader'
                 })
+            },
+            {
+                test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+                loader: 'url-loader?limit=1024'
             }
         ]
     },
