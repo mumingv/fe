bc522b7ff3a76b0a61c9e5fb873872e957fc7002
mumingv <mumingv@163.com>
2018-02-26 23:39:05 +0800

    10.3 单文件组件

---
+++ books/my-vue-book/ch10-webpack/01-webpack-install-demo/app.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>Hello {{ name }}</div>
+</template>
+<script>
+	export default {
+		// 等同于：
+		// data: function () {}
+		data () {
+			return {
+				name: 'Vue.js'
+			}
+		}
+	}
+</script>
+<style scoped>
+	div {
+		color: #f60;
+		font-size: 24px;
+	}
+</style>
--- books/my-vue-book/ch10-webpack/01-webpack-install-demo/main.js
+++ books/my-vue-book/ch10-webpack/01-webpack-install-demo/main.js
@@ -1,4 +1,15 @@
 // 不能写成：import 'style.css';
 // 重要提醒：即使将css打包到一个单独的文件中，这里的 import './style.css'; 也不能删除。
-import './style.css';
-document.getElementById('app').innerHTML = 'Hello webpack.';
+// import './style.css';
+// document.getElementById('app').innerHTML = 'Hello webpack.';
+
+// 导入Vue框架
+import Vue from 'vue';
+// 导入app.vue组件
+import App from './app.vue';
+
+// 创建Vue根实例
+new Vue({
+	el: '#app',
+	render: h => h(App)
+});
