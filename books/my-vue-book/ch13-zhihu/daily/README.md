2fd07f7959385c4b1ac34f571f826982e1258059
mumingv <mumingv@163.com>
2018-02-27 07:28:07 +0800

    从第10章拷贝项目作为开发基础

---
+++ books/my-vue-book/ch13-zhihu/daily/README.md
@@ -0,0 +1,27 @@
+## 操作步骤
+
+### 10.2.1 安装webpack与webpack-dev-server
+
+```
+npm init
+npm install webpack --save-dev
+npm install webpack-dev-server --save-dev
+npm install webpack-cli --save-dev （webpack新版本需要安装）
+```
+
+
+### 10.2.2 就是一个js文件而已
+
+```
+touch webpack.config.js
+```
+
+
+### 10.2.3 逐步完善配置文件
+
+```
+npm install css-loader --save-dev
+npm install style-loader --save-dev
+npm install extract-text-webpack-plugin --save-dev
+```
+
---
+++ books/my-vue-book/ch13-zhihu/daily/app.vue
@@ -0,0 +1,41 @@
+<template>
+	<!-- <div>Hello {{ name }}</div> -->
+	<div>
+		<v-title title="Vue组件化"></v-title>
+		<v-button @click="handleClick">点击按钮</v-button>
+		<p>
+			<img src="./images/image.png" style="width: 200px;">
+		</p>
+	</div>
+</template>
+<script>
+	// 导入组件
+	import vTitle from './title.vue';
+	import vButton from './button.vue';
+
+	export default {
+		// 等同于：
+		// data: function () {}
+		// data () {
+		// 	return {
+		// 		name: 'Vue.js'
+		// 	}
+		// }
+
+		components: {
+			vTitle,
+			vButton
+		},
+		methods: {
+			handleClick (e) {
+				console.log(e);
+			}
+		}
+	}
+</script>
+<style scoped>
+	div {
+		color: #f60;
+		font-size: 24px;
+	}
+</style>
