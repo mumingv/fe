4b9257cbfc706d70181576698a179d060e03ded2
mumingv <mumingv@163.com>
2018-04-28 07:05:03 +0800

    3.2.2 主题日报

--- books/my-vue-book/ch13-zhihu/daily/app.vue
+++ books/my-vue-book/ch13-zhihu/daily/app.vue
@@ -1,8 +1,18 @@
 <template>
 	<div class="daily">
 		<div class="daily-menu">
-			<div class="daily-menu-item">每日推荐</div>
-			<div class="daily-menu-item">主题日报</div>
+			<div class="daily-menu-item"
+				:class="{ on: type === 'recommend' }">每日推荐</div>
+			<div class="daily-menu-item"
+				:class="{ on: type === 'daily' }"
+				@click="showThemes = !showThemes">主题日报</div>
+			<ul v-show="showThemes">
+				<li v-for="item in themes">
+					<a :class="{ on: (item.id === themeId) && (type === 'daily') }"
+						@click="handleToTheme(item.id)"
+					>{{ item.name }}</a>
+				</li>
+			</ul>
 		</div>
 		<div class="daily-list">
 			<Item></Item>
@@ -10,3 +20,45 @@
 		<daily-article></daily-article>
 	</div>
 </template>
+<script>
+	// 这里的util.js中的.js可以省略
+	import $ from './libs/util';
+
+	export default {
+		data () {
+			return {
+				themes: [],
+				showThemes: true,
+				type: 'recommend',
+				// 文章列表
+				list: [],
+				themeId: 0
+			}
+		},
+		methods: {
+			getThemes () {
+				// http://news-at.zhihu.com/api/4/themes
+				$.ajax.get('themes').then(res => {
+					this.themes = res.others;
+				});
+			},
+			handleToTheme (id) {
+				// 改变菜单分类
+				this.type = 'daily';
+				// 设置当前点击子类的主题日报id
+				this.themeId = id;
+				// 清空中间栏的数据
+				this.list = [];
+				// 获取新菜单分类下的数据
+				// http://news-at.zhihu.com/api/4/theme/13
+				$.ajax.get('theme/' + id).then(res => {
+					this.list = res.stories.filter(item => item.type != 1);
+				});
+			}
+		},
+		mounted () {
+			// 初始化时调用
+			this.getThemes();
+		}
+	}
+</script>
