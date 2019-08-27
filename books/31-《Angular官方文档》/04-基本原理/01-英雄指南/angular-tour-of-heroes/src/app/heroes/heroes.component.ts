b7e24d498cdd6f78f45b29c223e3e87f3d32f7bf
mumingv <mumingv@163.com>
2019-08-27 15:15:31 +0800

    《Angular官方文档》

--- books/31-《Angular官方文档》/04-基本原理/01-英雄指南/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ books/31-《Angular官方文档》/04-基本原理/01-英雄指南/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,15 +9,15 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
-  
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
